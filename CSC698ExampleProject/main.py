504b94708d0d426796994e04c8c16f18db5fa8c9
Burned357Waffles <bwatanabe@sfsu.edu>
2025-06-12 17:20:21 -0700

    finished subtract function

--- CSC698ExampleProject/main.py
+++ CSC698ExampleProject/main.py
@@ -2,10 +2,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def subtract(a, b):
+    return a - b
 
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('Hello, World!')
+    result = subtract(10, 5)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
